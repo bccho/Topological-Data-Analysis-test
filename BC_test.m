904f16f3e052bb659e13e3be84efacb398d877c2
Byung Cheol Cho <bccho@princeton.edu>
2015-12-08 14:31:11 -0500

    Changes to interval filtering

--- BC_test.m
+++ BC_test.m
@@ -1,3 +1,5 @@
+clc
+
 %% Load images and 'sort' by numeral
 if (~exist('images', 'var'))
     images = loadMNISTImages('train-images-idx3-ubyte');
@@ -6,14 +8,23 @@ if (~exist('labels', 'var'))
     labels = loadMNISTLabels('train-labels-idx1-ubyte');
 end
 
+% Save memory
 n = 1000;
 images = images(:, 1:n);
 labels = labels(1:n);
 
+% Look at only particular numerals
+[sortedLabels, labelIndices] = sort(labels);
+labelIndices = labelIndices(sortedLabels == 8);
 ind = 1;
 
+%% Compute Betti intervals
 img = reshape_image(images(:, labelIndices(ind)), 0, false);
+[intervals, point_cloud] = BC_compute_intervals(img, 2, 10, 50, 1, 0.5, 4, 10000, false);
 
+%% Find relevant intervals
+intervals_dim0 = BC_filter_relevant_intervals(intervals, 0)
+intervals_dim1 = BC_filter_relevant_intervals(intervals, 1)
 
 %% Display results
 
