dc719ea86118d29856b4856737abca70c5ba8046
Byung Cheol Cho <bccho@princeton.edu>
2015-12-08 18:27:06 -0500

    Need a mechanism for displaying results

--- BC_varthreshold.m
+++ BC_varthreshold.m
@@ -16,19 +16,19 @@ sidelength = 28;
 
 % Look at only particular numerals
 [sortedLabels, labelIndices] = sort(labels);
-labelIndices = labelIndices(sortedLabels == 3);
+labelIndices = labelIndices(sortedLabels == 6);
 ind = 1;
 
 %% Loop through all of one particular numeral
+thresholds = [0, 0.25, 0.5, 0.75];
+nThresholds = length(thresholds);
+
 chart_dim0 = 5;
 chart_dim1 = 5;
-betti_numbers = cell(length(labelIndices));
+betti_numbers = cell(length(labelIndices), nThresholds);
 betti_chart = cell(chart_dim0, chart_dim1);
 
 for ind = 1:length(labelIndices)
-    thresholds = [0, 0.25, 0.5, 0.75];
-    nThresholds = length(thresholds);
-    
     img = reshape_image(images(:, labelIndices(ind)), 0, false);
     
     %% Loop through thresholds
@@ -44,7 +44,7 @@ for ind = 1:length(labelIndices)
         
         % Compute Betti numbers and increment relevant position on chart
         bn = [size(intervals_dim0, 1), size(intervals_dim1, 1)];
-        betti_numbers{ind} = bn;
+        betti_numbers{ind}{iTh} = bn;
 %         betti_chart{bn(1)+1, bn(2)+1} = [betti_chart{bn(1)+1, bn(2)+1}, ind];
         
         disp([num2str(ind), ' (', num2str(thres), ')', ': ', num2str(bn)])
@@ -52,28 +52,28 @@ for ind = 1:length(labelIndices)
 end
 
 %% Display results
-cnt = 0;
-for dim0 = 1:chart_dim0
-    for dim1 = 1:chart_dim1
-        arr = betti_chart{dim0, dim1};
-        n = length(arr);
-        if n > 0
-            cnt = cnt + 1;
-            nrows = floor(sqrt(n));
-            ncols = ceil(n / nrows);
-            
-            figure
-            for ii = 1:n
-                subplot(nrows,ncols,ii)
-                imagesc(reshape_image(images(:, labelIndices(arr(ii))), 0, false));
-                axis off
-                title(num2str(arr(ii)))
-            end
-            set(gcf,'name',num2str([dim0-1, dim1-1]),'numbertitle','off')
-        end
-    end
-end
-disp([num2str(cnt), ' types detected.'])
+% cnt = 0;
+% for dim0 = 1:chart_dim0
+%     for dim1 = 1:chart_dim1
+%         arr = betti_chart{dim0, dim1};
+%         n = length(arr);
+%         if n > 0
+%             cnt = cnt + 1;
+%             nrows = floor(sqrt(n));
+%             ncols = ceil(n / nrows);
+%             
+%             figure
+%             for ii = 1:n
+%                 subplot(nrows,ncols,ii)
+%                 imagesc(reshape_image(images(:, labelIndices(arr(ii))), 0, false));
+%                 axis off
+%                 title(num2str(arr(ii)))
+%             end
+%             set(gcf,'name',num2str([dim0-1, dim1-1]),'numbertitle','off')
+%         end
+%     end
+% end
+% disp([num2str(cnt), ' types detected.'])
 
 return
 
